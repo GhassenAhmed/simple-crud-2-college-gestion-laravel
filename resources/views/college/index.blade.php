21063673bc8883051f4848a58db405313a59eee0
ghassen <ghassenahmed74@gmail.com>
2023-01-25 23:05:11 +0100

    install bootstrap

--- resources/views/college/index.blade.php
+++ resources/views/college/index.blade.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>Listes</title>
 </head>
 <body>
     <h1>Ghassen</h1>
+    <button class="btn btn-danger">test</button>
 </body>
 </html>

87414cd1f2293642c0c83179a5b160b976b74755
ghassen <ghassenahmed74@gmail.com>
2023-01-31 17:03:12 +0100

    design

--- resources/views/college/index.blade.php
+++ resources/views/college/index.blade.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+     crossorigin="anonymous"></script>
     <title>Listes</title>
 </head>
 <style>
@@ -13,7 +16,40 @@
         padding: 0;
         background-color: #e5eaf5;
     }
+    a.navbar-brand{
+        color: #3F0071;
+        font-size: 30px;
+        font-family:Roboto Slab;
+    }
+    a.nav-link{
+        color:#6E85B7;
+        font-size: 25px;
+        margin-left: 30px;
+        font-family:Roboto Slab;
+    }
+    
 </style>
 <body>
+    
+        <nav class="navbar navbar-expand-lg px-5 py-2">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">College</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    
+    
 </body>
 </html>

8e4865fc61abb720aad556113e79723e8e69d86a
ghassen <101054191+GhassenAhmed@users.noreply.github.com>
2023-02-10 19:14:34 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ use App\Http\Controllers\EnseignantController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () {
-    return view('college.index');
-});
+
 Route::get('/indexDepartement', function () {
     return view('college.indexDepartement');
 });

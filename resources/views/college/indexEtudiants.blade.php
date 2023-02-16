8385bdee74a5ecea31d691e6cb11ce3a7a008f05
ghassen <ghassenahmed74@gmail.com>
2023-02-16 09:32:14 +0100

    add router create etudiant

--- resources/views/college/indexEtudiants.blade.php
+++ resources/views/college/indexEtudiants.blade.php
@@ -67,7 +67,7 @@
           </nav>
           <div class="container mt-5">
             <h3 class="py-4 ">Etudiants</h3>
-            <a href="" class="btn btn-outline-success float-end">Add</a>
+            <a href="{{url('/etudiant')}}" class="btn btn-outline-success float-end">Add</a>
             <table class="table">
               <thead>
                 <tr>

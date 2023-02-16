7b41face7eea0cf51e4ca35246f4157e78bed578
ghassen <ghassenahmed74@gmail.com>
2023-02-16 09:56:55 +0100

    add formulaire

--- resources/views/create/Etudiant.blade.php
+++ resources/views/create/Etudiant.blade.php
@@ -4,9 +4,114 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN"
+     crossorigin="anonymous"></script>
     <title>Add</title>
 </head>
+<style>
+    *{
+        margin: 0;
+        padding: 0;
+        
+    }
+    body{
+        background-color: #FAFAFA;
+    }
+    h3{
+        
+      color: #811616;
+      font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      text-align: center;
+    }
+    .form {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  background-color: white;
+  padding: 2.5em;
+  border-radius: 25px;
+  transition: .4s ease-in-out;
+  box-shadow: rgba(0, 0, 0, 0.4) 1px 2px 2px;
+  width: 700px;
+  justify-content: center;
+  align-items: center;
+  
+}
+
+.form:hover {
+  transform: translateX(-0.5em) translateY(-0.5em);
+  border: 1px solid #171717;
+  box-shadow: 10px 10px 0px #666666;
+  
+}
+
+.heading {
+  color: black;
+  padding-bottom: 2em;
+  text-align: center;
+  font-weight: bold;
+}
+
+.input {
+  border-radius: 5px;
+  border: 1px solid whitesmoke;
+  background-color: whitesmoke;
+  outline: none;
+  padding: 0.7em;
+  transition: .4s ease-in-out;
+  width: 500px;
+}
+
+.input:hover {
+  box-shadow: 6px 6px 0px #969696,
+             -3px -3px 10px #ffffff;
+}
+
+.input:focus {
+  background: #ffffff;
+  box-shadow: inset 2px 5px 10px rgba(0,0,0,0.3);
+}
+
+.form .btn {
+  margin-top: 2em;
+  align-self: center;
+  padding: 0.7em;
+  padding-left: 1em;
+  padding-right: 1em;
+  border-radius: 10px;
+  border: none;
+  color: black;
+  transition: .4s ease-in-out;
+  box-shadow: rgba(0, 0, 0, 0.4) 1px 1px 1px;
+}
+
+.form .btn:hover {
+  box-shadow: 6px 6px 0px #969696,
+             -3px -3px 10px #ffffff;
+  transform: translateX(-0.5em) translateY(-0.5em);
+}
+
+.form .btn:active {
+  transition: .2s;
+  transform: translateX(0em) translateY(0em);
+  box-shadow: none;
+}
+</style>
 <body>
-    <h1></h1>
+    <div class="container">
+        <a href="{{url('/indexEtudiants')}}" class="btn btn-info mt-5">Go back</a>
+        <h3 class="">Add Etudiant</h3>
+        <form class="form mt-5 py-5" >
+            <p class="heading">Login</p>
+            <input class="input" placeholder="Cin" type="text">
+            <input class="input" placeholder="Nom" type="text"> 
+            <input class="input" placeholder="Prenom" type="text">
+            <input class="input" placeholder="Departement" type="text"> 
+            <button class="btn">Add</button>
+        </form>
+    </div>
+   
 </body>
 </html>
